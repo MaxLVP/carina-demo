b4891ff246519f2cbf99ae8f82328f6e2348786b
maxza <maxzah99@gmail.com>
2023-02-24 15:10:03 +0300

    add dummyJSOON todo by user test

--- src/test/java/com/qaprosoft/carina/demo/DummyJSONTest.java
+++ src/test/java/com/qaprosoft/carina/demo/DummyJSONTest.java
@@ -1,10 +1,9 @@
 package com.qaprosoft.carina.demo;
 
+import com.qaprosoft.apitools.validation.JsonCompareKeywords;
 import com.qaprosoft.carina.core.foundation.IAbstractTest;
-import com.qaprosoft.carina.demo.api.DummyJSON.DeleteToDoMethod;
-import com.qaprosoft.carina.demo.api.DummyJSON.GetToDoMethod;
-import com.qaprosoft.carina.demo.api.DummyJSON.PostToDoMethod;
-import com.qaprosoft.carina.demo.api.DummyJSON.PutToDoMethod;
+import com.qaprosoft.carina.demo.api.DummyJSON.*;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
@@ -19,6 +18,14 @@ public class DummyJSONTest implements IAbstractTest {
         GetToDoMethod getToDoMethod = new GetToDoMethod();
         getToDoMethod.callAPIExpectSuccess();
         getToDoMethod.validateResponse();
+    }
+
+    @Test
+    public void testGetToDoByUser() {
+        GetUserToDoMethod getUserToDoMethod = new GetUserToDoMethod();
+        getUserToDoMethod.callAPIExpectSuccess();
+        getUserToDoMethod.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey() + "todos");
+        getUserToDoMethod.validateResponseAgainstSchema("api/dummyJson/todo/_get/rs.schema");
     }
 
     @Test
