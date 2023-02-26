a21aedf337c56ae45dfc29ba53288c56984d3b1f
maxza <maxzah99@gmail.com>
2023-02-26 13:16:32 +0300

    second test

--- src/main/java/com/qaprosoft/carina/demo/web/sauce_demo/pages/HomePage.java
+++ src/main/java/com/qaprosoft/carina/demo/web/sauce_demo/pages/HomePage.java
@@ -2,6 +2,7 @@ package com.qaprosoft.carina.demo.web.sauce_demo.pages;
 
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
@@ -12,7 +13,10 @@ import java.util.Random;
 
 public class HomePage extends AbstractPage {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final String ERROR_WINDOW_XPATH = "//h3[@data-test='error']";
+    private static final String ERROR_WINDOW_CLOSE_BUTTON_XPATH = "//button[@class='error-button']";
     private static final Integer STANDARD_USER = 1;
+    private static final Integer LOCKED_OUT_USER = 2;
 
     @FindBy(id = "user-name")
     private ExtendedWebElement usernameInput;
@@ -47,5 +51,33 @@ public class HomePage extends AbstractPage {
         passwordInput.type(passwords[pass]);
         loginButton.click();
         return new InventoryPage(getDriver());
+    }
+
+    public void blockedAuthorization() {
+        LOGGER.info("Authorization on site");
+        String[] usernames = loginText.getText().split("\n");
+        LOGGER.info("Insert locked out username");
+        usernameInput.click();
+        usernameInput.type(usernames[LOCKED_OUT_USER]);
+        String[] passwords = password.getText().split("\n");
+        int pass = new Random().nextInt(passwords.length - 1) + 1;
+        LOGGER.info("Insert password");
+        passwordInput.click();
+        passwordInput.type(passwords[pass]);
+        loginButton.click();
+    }
+
+    public boolean isErrorDisplay() {
+        ExtendedWebElement errorWindow = findExtendedWebElement(By.xpath(ERROR_WINDOW_XPATH));
+        if (errorWindow == null) {
+            return false;
+        }
+        return errorWindow.isElementPresent();
+    }
+
+    public void closeErrorDisplay() {
+        LOGGER.info("Close error window");
+        ExtendedWebElement closeErrorWindowBtn = findExtendedWebElement(By.xpath(ERROR_WINDOW_CLOSE_BUTTON_XPATH));
+        closeErrorWindowBtn.click();
     }
 }
--- src/test/java/com/qaprosoft/carina/demo/SauceDemoTest.java
+++ src/test/java/com/qaprosoft/carina/demo/SauceDemoTest.java
@@ -18,11 +18,23 @@ public class SauceDemoTest implements IAbstractTest {
         InventoryPage inventoryPage = homePage.authorization();
         Assert.assertTrue(inventoryPage.isPageOpened(), "Inventory page is not opened!");
         Menu menu = inventoryPage.openMenu();
+        Assert.assertTrue(menu.isUIObjectPresent(), "Menu is not opened");
         AboutPage aboutPage = menu.openAboutPage();
         Assert.assertTrue(aboutPage.isPageOpened(), "About page is not opened!");
         getDriver().navigate().back();
         Assert.assertTrue(inventoryPage.isPageOpened(), "Inventory page is not opened!");
         menu.logout();
         Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
+    }
+
+    @Test
+    public void testBlockedUser() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
+        homePage.blockedAuthorization();
+        Assert.assertTrue(homePage.isErrorDisplay(), "Error window is not displayed");
+        homePage.closeErrorDisplay();
+        Assert.assertFalse(homePage.isErrorDisplay(), "Error window is still display");
     }
 }
